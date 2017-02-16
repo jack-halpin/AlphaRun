4251296449a200bccb6ec5c02ef0367988067495
Jack Halpin <jhalpin92@gmail.com>
2017-02-16 17:01:31 +0000

    Moved permission checking to MainActivity

--- app/src/main/java/com/alpharun/jack/alpharun/MainActivity.java
+++ app/src/main/java/com/alpharun/jack/alpharun/MainActivity.java
@@ -1,16 +1,37 @@
 package com.alpharun.jack.alpharun;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Code for Permission request used to check if user has given permission for GPS
+    protected static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Before the app runs we want to make sure that the app has the permission to track location.
+        //For the sake of running accuracy we're going to be using FINE_LOCATION
+        //Android 23 and onwards: Need to check permissions at runtime.
+        int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+
+        //TODO: Hande the event that the user denies access to GPS
+        //If the permission is denied we have to request it.
+        if (permissionCheck == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
 
         //Get the database ready
         //DBManager db = new DBManager();
@@ -19,5 +40,29 @@ public class MainActivity extends AppCompatActivity {
     public void startRunCallback(View view){
         Intent intent = new Intent(this, RunActivity.class);
         startActivity(intent);
+    }
+
+    //Callback method for when the user chooses to set the permission
+    //TODO: Move asking permission to the beginning of the app (MainActivity)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    Toast.makeText(this, "You need location services to use this app", Toast.LENGTH_LONG).show();
+
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
